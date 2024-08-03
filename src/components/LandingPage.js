da74b42cbf780f7961e4f0206b36cae071207e36
razzzz1 <ryanmaiyo1@gmail.com>
2024-08-03 14:49:45 +0300

    added the landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LandingPage.css';
+
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="left-panel">
+        <h1>Welcome to MY DUKA</h1>
+
+        
+      </div>
+      <div className="right-panel">
+        <h2>Continue As</h2>
+        <button>merchant</button>
+        <button>admin</button>
+        <button>clerk</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;

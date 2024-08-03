f5e0e63a3ab359f113b05a76e9b5867a0be78a55
razzzz1 <ryanmaiyo1@gmail.com>
2024-08-03 15:10:26 +0300

    added background picture

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './LandingPage.css';
+import myImage from '../assets/images/pexels-rebrand-cities-581004-1367272.jpg';
+
 
 
 const LandingPage = () => {
@@ -7,6 +9,7 @@ const LandingPage = () => {
     <div className="landing-page">
       <div className="left-panel">
         <h1>Welcome to MY DUKA</h1>
+        <img src={myImage} alt ="Background" />
 
         
       </div>
